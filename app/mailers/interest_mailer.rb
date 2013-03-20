f4be9ab408fe8ab4875a29df0f9301d90d850819
sstevens13 <shawn.r.stevens@gmail.com>
2013-03-20 18:27:30 -0500

    proper link added /

--- app/mailers/interest_mailer.rb
+++ app/mailers/interest_mailer.rb
@@ -4,7 +4,7 @@ class InterestMailer < ActionMailer::Base
   def interested_email(user_of_interest, hidden_user)
     @user_of_interest = user_of_interest.username
     @hidden_user = hidden_user.username
-    @url  = "http://hellostranger.info#{user_path(hidden_user.id)}"
+    @url  = "http://hellostranger.info/#{user_path(hidden_user.id)}"
     mail(:to => user_of_interest.email, :subject => "#{hidden_user.username} is interested in you!")
   end
 end
