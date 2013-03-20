650f299f92d0857b21d0fc118e20a02bc0533c73
sstevens13 <shawn.r.stevens@gmail.com>
2013-03-20 17:59:27 -0500

    fixed visibles controller to evaluate properly, fixed mailer

--- app/controllers/visibles_controller.rb
+++ app/controllers/visibles_controller.rb
@@ -4,12 +4,12 @@ class VisiblesController < ApplicationController
     @user_of_interest = params[:user_id]
     @hidden_user =session[:user_id]
 
-    if Visible.where(hidden_user: @hidden_user, user_of_interest: @user_of_interest)
+    if !Visible.where(hidden_user: @hidden_user, user_of_interest: @user_of_interest).blank?
       redirect_to user_path(@user_of_interest), 
         notice: "You've already allowed #{User.find(params[:user_id]).username} to see you!"   
     else
-      Visible.create(hidden_user: session[:user_id], user_of_interest: @user_of_interest)
-      InterestMailer.interested_email(@user_of_interest, @hidden_user).deliver
+      Visible.create(hidden_user: @hidden_user, user_of_interest: @user_of_interest)
+      InterestMailer.interested_email(User.find(@user_of_interest), User.find(@hidden_user)).deliver
       redirect_to user_path(@user_of_interest), notice: 
         "You've allowed #{User.find(params[:user_id]).username} to see you!" 
     end
